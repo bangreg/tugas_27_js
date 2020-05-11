b0359b46d555305a1a40606062036fc64f94eeda
niomicsam <biskuitkelapa@gmail.com>
2020-05-11 09:46:43 +0700

    Tugas 27 JS
    Gabungkanlah kata berikut ini menjadi satu variabel :
    
        “No”,” rumah saya”,30, “A”
        “Jalan pangeran Tirtayasa No”, 70

---
+++ tugas27.js
@@ -0,0 +1,15 @@
+function satuVariabel(){
+  var a='No ';
+  var b='rumah saya ';
+  var c=30;
+  var d=' A';
+
+  var e='Jalan pangeran Tirtayasa No '
+  var f=70;
+
+  c1=String(c);
+  f1=String(f);
+  console.log(a+b+c1+d);
+  console.log(e+f1);
+}
+satuVariabel();
